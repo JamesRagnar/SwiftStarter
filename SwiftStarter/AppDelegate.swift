baf9190411bc933663ac5dfe7a292059415c8709
JamesRagnar <james.harquail@hotmail.com>
2017-05-26 22:31:27 -0400

    Added root navigation controller

--- SwiftStarter/AppDelegate.swift
+++ SwiftStarter/AppDelegate.swift
@@ -15,7 +15,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         rootWindow = RootWindow(frame: UIScreen.main.bounds)
-        rootWindow?.rootViewController = RootViewController()
         rootWindow?.makeKeyAndVisible()
         return true
     }
--- SwiftStarter/RootWindow.swift
+++ SwiftStarter/RootWindow.swift
@@ -10,4 +10,15 @@ import UIKit
 
 class RootWindow: UIWindow {
 
+    let navigationController: UINavigationController
+
+    override init(frame: CGRect) {
+        navigationController = UINavigationController(rootViewController: RootViewController())
+        super.init(frame: frame)
+        rootViewController = navigationController
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
